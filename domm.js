478e0922d6d2f0c70370fa594293ae167ba81890
Harishbpatil <hp777489@gmail.com>
2023-11-09 14:22:14 +0530

    changes

--- domm.js
+++ domm.js
@@ -1,7 +1,14 @@
-const thirdItem = document.querySelector("#items li:nth-child(3)");
-thirdItem.style.backgroundColor = "green";
+//const thirdItem = document.querySelector("#items li:nth-child(3)");
+//thirdItem.style.backgroundColor = "green";
 
-const listItems = document.querySelectorAll("#items li");
-listItems.forEach((item) => {
-  item.style.fontWeight = "bold";
-});
+///const listItems = document.querySelectorAll("#items li");
+//listItems.forEach((item) => {
+//item.style.fontWeight = "bold";});
+
+//GETELEMNTSBYTAGNAME//
+var li = document.getElementsByTagName("li");
+console.log(li);
+console.log(li[1]);
+li[1].textContent = "Hello 2";
+li[1].style.fontWeight = "bold";
+li[1].style.backgroundColor = "yellow";
