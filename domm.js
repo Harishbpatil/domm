e98bf2bd2acccb3de2861b3c27c08cea238aec19
Harishbpatil <hp777489@gmail.com>
2023-11-09 19:21:35 +0530

    hello world before itemList

--- domm.js
+++ domm.js
@@ -14,7 +14,7 @@ li[1].style.fontWeight = "bold";
 li[1].style.backgroundColor = "yellow";*/
 
 //QuaerySelection
-var header = document.querySelector("#main-header");
+/*var header = document.querySelector("#main-header");
 header.style.borderBottom = "solid 4px #ccc";
 
 var input = document.querySelector("input");
@@ -43,4 +43,88 @@ for (var i = 0; i < odd.length; i++) {
 
 var itemList = document.querySelector("#items");
 var thirdItem = itemList.children[2];
-thirdItem.style.display = "none";
+thirdItem.style.display = "none";*/
+
+//traverse the DOM
+/*var itemList = document.querySelector("#items");
+//parentNode
+console.log(itemList.parentNode);
+itemList.parentNode.style.backgroundColor = "green";
+console.log(itemList.parentNode.parentNode);*/
+
+//parentElement
+/*console.log(itemList.parentElement);
+itemList.parentElement.style.backgroundColor = "green";
+console.log(itemList.parentElement.parentElement);*/
+
+//childNodes
+//console.log(itemList.childNodes);
+/*console.log(itemList.children);
+console.log(itemList.children[1]);
+itemList.children[1].style.backgroundColor = "green";
+
+console.log(itemList.firstChild);*/
+//firstChildElement
+/*console.log(itemList.firstElementChild);
+itemList.firstElementChild.textContent = "hello 1";*/
+
+//lastChild
+//console.log(itemList.lastChild);
+
+//lastChildElement
+/*console.log(itemList.lastElementChild);
+itemList.lastElementChild.textContent = "hello 4";*/
+
+/*var itemList = document.querySelector("#items");
+var lastItem = itemList.lastElementChild;
+lastItem.textContent = "hello 4";*/
+
+//nextSibling
+//console.log(itemList.nextSibling);
+
+//nextElementSibling
+//console.log(itemList.nextElementSibling);
+
+//previousSibling
+/*console.log(itemList.previousSibling);
+itemList.previousElementSibling.style.backgroundColor = "green";*/
+
+//createElement
+
+//create a div
+var newDiv = document.createElement("div");
+//Add Class
+newDiv.className = "hello";
+
+//Add id
+newDiv.id = "Hello1";
+
+//Add attribute
+newDiv.setAttribute("data-name", "hello");
+
+//create text node
+
+var newDivText = document.createTextNode("Hello World");
+
+//Add text tio Div
+newDiv.appendChild(newDivText);
+
+var container = document.querySelector("header .container");
+var h1 = document.querySelector("header h1");
+
+console.log(newDiv);
+
+newDiv.style.fontSize = "30px";
+
+container.insertBefore(newDiv, h1);
+
+var itemList = document.querySelector("#items");
+
+// Create a new list item element
+var newItem = document.createElement("li");
+
+// Set the text content of the new item
+newItem.textContent = "Hello world";
+
+// Insert the new item before the first child of the items list
+itemList.insertBefore(newItem, itemList.firstChild);
