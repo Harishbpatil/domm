e53b55d16a0229a98e4d4078ff7c7b938765cc2d
Harishbpatil <hp777489@gmail.com>
2023-11-10 17:51:06 +0530

    add user input details on screen and local storage

--- domm.js
+++ domm.js
@@ -1,9 +1,61 @@
-let myObj = {
-  name: " ",
-  age: " ",
-};
+// Serialize an object to JSON and store it in local storage
+function serialize(key, obj) {
+  localStorage.setItem(key, JSON.stringify(obj));
+}
 
-let myObj_serialized = JSON.stringify(myObj);
-localStorage.setItem("myObj", myObj_serialized);
-let myObj_deserialized = JSON.parse(localStorage.getItem("myObj"));
-console.log(myObj_deserialized);
+// Deserialize a JSON string from local storage and convert it to an object
+function deserialize(key) {
+  const storedData = localStorage.getItem(key);
+  return storedData ? JSON.parse(storedData) : null;
+}
+
+// Function to display user details on the screen
+function displayUserDetails(userDetails) {
+  // Clear the existing user list on the screen
+  const userList = document.getElementById("users");
+  userList.innerHTML = "";
+
+  // Iterate over user details and add them to the screen
+  userDetails.forEach((user) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = `${user.name}: ${user.email}`;
+    userList.appendChild(listItem);
+  });
+}
+
+// Put DOM elements into variables
+const myForm = document.getElementById("my-form");
+
+// Listen for form submit
+myForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  // Get existing user details from local storage
+  const existingUserDetails = deserialize("userDetails") || [];
+
+  // Get user details from the form
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+
+  // Check if name and email are not empty
+  if (name.trim() !== "" && email.trim() !== "") {
+    // Create a user object with the details
+    const userDetails = {
+      name: name,
+      email: email,
+    };
+
+    // Add the new user details to the existing list
+    existingUserDetails.push(userDetails);
+
+    // Serialize and store the updated user details in local storage
+    serialize("userDetails", existingUserDetails);
+
+    // Display the user details on the screen
+    displayUserDetails(existingUserDetails);
+
+    // Clear the form fields
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+  }
+});
