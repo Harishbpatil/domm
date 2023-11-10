a5b2271a7bd0a5bca1317cbd52f15d8e9f2a0724
Harishbpatil <hp777489@gmail.com>
2023-11-10 16:55:33 +0530

    use object local storage

--- domm.js
+++ domm.js
@@ -1,27 +1,9 @@
-// Put DOM elements into variables
-const myForm = document.getElementById("my-form");
+let myObj = {
+  name: " ",
+  age: " ",
+};
 
-// Listen for form submit
-myForm.addEventListener("submit", function (e) {
-  e.preventDefault();
-
-  // Get user details from the form
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-
-  // Check if name and email are not empty
-  if (name.trim() !== "" && email.trim() !== "") {
-    // Create a user object with the details
-    const userDetails = {
-      name: name,
-      email: email,
-    };
-
-    // Convert the user details to JSON and store in local storage
-    localStorage.setItem("userDetails", JSON.stringify(userDetails));
-
-    // Display the local storage key and value in the console
-    console.log("localStorage Key: userDetails");
-    console.log("localStorage Value:", localStorage.getItem("userDetails"));
-  }
-});
+let myObj_serialized = JSON.stringify(myObj);
+localStorage.setItem("myObj", myObj_serialized);
+let myObj_deserialized = JSON.parse(localStorage.getItem("myObj"));
+console.log(myObj_deserialized);
